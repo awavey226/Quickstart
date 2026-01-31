eada7f47c1e83f76ab16452b3274038c09f3c4c1
LARobotics <awavey226@gmail.com>
2026-01-31 13:53:01 -0500

    Added in Directions for Monday

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -7,13 +7,21 @@ import com.pedropathing.paths.PathConstraints;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Constants {
-    public static FollowerConstants followerConstants = new FollowerConstants();
+    public static FollowerConstants followerConstants = new FollowerConstants().mass(); //Add in mass here in KG
+
+    //Copy over mecanum constants here from the pedropathing tuning page
+    //Then change the motor names and adjust the motor directions
+
+
+    //Copy over pinport constants here from pedropathing tuning page
+    //Add in offsets in inches, use values from CAD
 
     public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
 
     public static Follower createFollower(HardwareMap hardwareMap) {
         return new FollowerBuilder(followerConstants, hardwareMap)
                 .pathConstraints(pathConstraints)
+                //Add in mecanum drive constants here
                 .build();
     }
 }
